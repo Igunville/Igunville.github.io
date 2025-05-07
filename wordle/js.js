b51edf3d9b9e1d316c227c0f37d8ef8284333ae2
Isaac Gunville <Isaac@gunvilles.com>
2025-05-07 09:19:28 -0400

    progress

--- wordle/js.js
+++ wordle/js.js
@@ -1,9 +1,12 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-const rows = 6;
+const word = "Pears";
+const rows = 5;
 const cols = 5;
 const padding = 5;
+let attempts = 0;
+let currentGuess = "";
 const squareSize = (canvas.width - padding * (cols + 1)) / cols;
 
 for (let row = 0; row < rows; row++) {
@@ -13,4 +16,15 @@ for (let row = 0; row < rows; row++) {
         ctx.strokeRect(x, y, squareSize, squareSize);
     }
 }
+
+function submitGuess() [
+	if (attempts >=rows || currentGuess.length !== cols)
+
+	let guess = currentGuess.toUpperCase();
+	currentGuess = "";
+
+//I need to make a keylistner next time i work on this
+//i need to make it so it can change colors when you enter it
+// make the attmetpts go up if wrong
+// make it so when you press keys it enters
 
